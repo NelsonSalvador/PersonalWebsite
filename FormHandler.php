a412f5159fe3f7c122fc4782dc9dd084d28d9a4d
Nelson Salvador <nelsonsalvadorzx@gmail.com>
2022-02-21 17:47:56 +0000

    Contact Me

---
+++ FormHandler.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['name'];
+$visitor_email; = $_POST['email'];
+$subject; = $_POST['subject'];
+$message; = $_POST['message'];
+
+$email_from = 'nelsonsalvadorzx@gmail.com';
+$email_subject = 'Personal Website reply !';
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "User subject: $subject.\n".
+                "User message: $message.\n";
+
+$to = 'nelsonsalvadorzx@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: Index.html")
+
+?>
